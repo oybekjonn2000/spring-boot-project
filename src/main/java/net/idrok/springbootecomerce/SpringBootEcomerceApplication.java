f3a47369d8f641fdbf017ac726a8e7548e254010
OYBEK <botirov_2000@bk.ru>
2022-04-26 14:36:45 +0500

    first commit

---
+++ src/main/java/net/idrok/springbootecomerce/SpringBootEcomerceApplication.java
@@ -0,0 +1,13 @@
+package net.idrok.springbootecomerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEcomerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootEcomerceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/idrok/springbootecomerce/SpringBootEcomerceApplicationTests.java
@@ -0,0 +1,13 @@
+package net.idrok.springbootecomerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEcomerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
