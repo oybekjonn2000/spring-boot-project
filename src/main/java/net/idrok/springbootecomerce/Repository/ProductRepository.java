91b27438844df7a19b0c9f2147ac0cc09beff2c6
OYBEK <botirov_2000@bk.ru>
2022-04-27 12:44:26 +0500

    crossOrigin added

--- src/main/java/net/idrok/springbootecomerce/Repository/ProductRepository.java
+++ src/main/java/net/idrok/springbootecomerce/Repository/ProductRepository.java
@@ -3,7 +3,9 @@ package net.idrok.springbootecomerce.Repository;
 import net.idrok.springbootecomerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Repository
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
