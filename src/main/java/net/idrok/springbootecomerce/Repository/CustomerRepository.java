38807f881e0ed300f060c27246658976b63e30e0
OYBEK <botirov_2000@bk.ru>
2022-05-20 00:34:43 +0500

    247-lesson coming

--- src/main/java/net/idrok/springbootecomerce/Repository/CustomerRepository.java
+++ src/main/java/net/idrok/springbootecomerce/Repository/CustomerRepository.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    Customer findByEmail(String theEmail);
 
 
 }
