c9ff7f80192a1c6fd5d070f3e22580cf60ea70a6
OYBEK <botirov_2000@bk.ru>
2022-05-27 18:27:49 +0500

    271

---
+++ src/main/java/net/idrok/springbootecomerce/config/SecurityConfiguration.java
@@ -0,0 +1,30 @@
+package net.idrok.springbootecomerce.config;
+
+import com.okta.spring.boot.oauth.Okta;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+      // protect endpoint /api/orders
+
+        http.authorizeHttpRequests()
+                .antMatchers("/api/orders/**")
+                .authenticated()
+                .and()
+                .oauth2ResourceServer()
+                .jwt();
+
+        // add CORS filters
+        http.cors();
+
+        // force a non-empty response body for 401 's to make the response more friednly
+        Okta.configureResourceServer401ResponseBody(http);
+
+}
+}
